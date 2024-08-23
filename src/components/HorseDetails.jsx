d0999d74050576cc188c0ba58c1072191e852e1d
twattley <wattley83@gmail.com>
2024-08-23 16:52:33 +0100

    wip

--- src/components/HorseDetails.jsx
+++ src/components/HorseDetails.jsx
@@ -13,7 +13,7 @@ export function HorseDetails({
         className="text-2xl pt-2 font-bold mb-2 cursor-pointer"
         onClick={() => toggleHorseVisibility(horse.horse_id)}
       >
-        <div className="grid grid-cols-[30px,30px,minmax(100px,auto),80px,80px,80px,80px,80px,80px,80px,80px] gap-2">
+        <div className="grid grid-cols-[30px,30px,minmax(100px,auto),80px,80px,80px,80px,80px,80px,80px,80px,80px] gap-2">
           <button
             className="bg-gray-400 hover:bg-gray-900 text-white font-bold py-1 px-2 rounded"
             onClick={(e) => {
@@ -73,6 +73,13 @@ export function HorseDetails({
             }`}
           >
             {horse.todays_betfair_place_sp}
+          </span>
+          <span
+            className={`bg-gray-200 px-2 py-1 rounded ${
+              !visibleHorses[horse.horse_id] ? "bg-gray-200" : ""
+            }`}
+          >
+            {horse.todays_simulated_price}
           </span>
           <span
             className={`bg-green-400 text-black px-2 py-1 rounded ${
