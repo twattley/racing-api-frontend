076853f498032f3bc9b682bd170c7b595afcad6a
twattley <wattley83@gmail.com>
2024-09-23 20:08:16 +0100

    wip

--- src/components/HorseDetails.jsx
+++ src/components/HorseDetails.jsx
@@ -15,7 +15,14 @@ export function HorseDetails({
         className="text-2xl pt-2 font-bold mb-2 cursor-pointer"
         onClick={() => toggleHorseVisibility(horse.horse_id)}
       >
-        <div className="grid grid-cols-[30px,30px,minmax(100px,auto),80px,80px,80px,80px,80px,80px,80px,80px,80px] gap-2">
+        <div className="grid grid-cols-[55px,30px,30px,minmax(240px,auto),55px,55px,65px,65px,55px,55px,55px,55px,55px] gap-2">
+          <span
+            className={`border border-gray-300 px-2 py-1 rounded ${
+              !visibleHorses[horse.horse_id] ? "bg-white-200" : ""
+            }`}
+          >
+            {horse.todays_horse_number}
+          </span>
           <button
             className="bg-gray-400 hover:bg-gray-900 text-white font-bold py-1 px-2 rounded"
             onClick={(e) => {
@@ -77,18 +84,8 @@ export function HorseDetails({
             {horse.todays_betfair_place_sp}
           </span>
           <span
-            className={`px-2 py-1 rounded flex items-center justify-center ${
-              !visibleHorses[horse.horse_id]
-                ? "bg-gray-200"
-                : parseFloat(horse.todays_price_change) < -5
-                ? "bg-orange-400"
-                : parseFloat(horse.todays_price_change) < 0
-                ? "bg-green-200"
-                : parseFloat(horse.todays_price_change) > 5
-                ? "bg-blue-300"
-                : parseFloat(horse.todays_price_change) > 2
-                ? "bg-blue-200"
-                : "bg-gray-200"
+            className={`bg-gray-200 px-2 py-1 rounded ${
+              !visibleHorses[horse.horse_id] ? "bg-gray-200" : ""
             }`}
           >
             {horse.todays_price_change}
